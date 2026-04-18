71855cfc9950c57ba733d994b809fe02a776bf50
Jan Kupčič <kupcic.jan@gmail.com>
2026-04-19 01:03:34 +0200

    Deployment Candidate 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    devIndicators: false // 'as any' prepreči TS2353 napako, če se tipi v tvoji verziji razlikujejo
 };
 
 export default nextConfig;
